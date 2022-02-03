c395da5ea42663462df744d2c619acd13ccd02a5
Johannes Weber <johannes@drweber.de>
2022-02-02 20:33:09 -0800

    implement config.json

--- run.js
+++ run.js
@@ -1,24 +1,10 @@
+const fs = require('fs');
 const dbHelpers = require('./dbHelpers');
 
-let columns;
+//Read table data specification from config.json
+const tables = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
-//Make seed file for users table
-columns = {
-  "name": { type: 'name', length: 30},
-};
-dbHelpers.writeSeedFile('users', columns, 2000);
-
-//Make seed file for quizzes table
-columns = {
-  "title": { type: 'words', length: 5 },
-  "user_id": { type: 'integer', max:100},
-};
-dbHelpers.writeSeedFile('quizzes', columns, 200);
-
-//Make seed file for options table
-columns = {
-  "question_id": { type: 'integer', max:200},
-  "content" : {type: 'words', length: 3}
-};
-dbHelpers.writeSeedFile('options', columns, 1000);
-
+//Write each file
+for (const table of tables) {
+  dbHelpers.writeSeedFile(table.name, table.columns, table.noRecords);
+}
